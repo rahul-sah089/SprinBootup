40e6f761eaf45442dce1ad3249676b31e9a14204
rahul.sah089 <rahul.sah089@gmail.com>
2019-01-24 06:13:54 +0530

    adding authentication server to spring boot applications

--- spring-boot-h2/src/main/java/com/DemoApplication.java
+++ spring-boot-h2/src/main/java/com/DemoApplication.java
@@ -3,10 +3,11 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
+@EnableResourceServer
 @SpringBootApplication
 @ComponentScan(basePackages="com.example")
 public class DemoApplication {
